f2f103984273bfd841ce0b423a8bff7eb67ae873
JeanCarlo <JeanCarlo@Note-JC>
2015-06-06 15:38:50 -0300

    Exclusão de veiculo funcionando PS: excluir banco e criar novamente e mudar senha do BD caso necessario

--- src/java/Controller/BD_Veiculo.java
+++ src/java/Controller/BD_Veiculo.java
@@ -8,6 +8,7 @@ package Controller;
 import Model.B_Veiculo;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 /**
@@ -15,6 +16,7 @@ import javax.persistence.TypedQuery;
  * @author JeanCarlo
  */
 public class BD_Veiculo {
+
     public void salvar(B_Veiculo v) {
 
         try {
@@ -54,45 +56,42 @@ public class BD_Veiculo {
 
         try {
             EntityManager entityManager = Conexao.getEntityManager();
-
             entityManager.getTransaction().begin();
-
-            entityManager.persist(v);
-
+            Query query = entityManager.createQuery("DELETE FROM veiculos v WHERE v.id = :n");
+            int deletedCount = query.setParameter("n", v.getId()).executeUpdate();
             entityManager.getTransaction().commit();
-
             entityManager.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public List<B_Veiculo> listar(){
-     
+    public List<B_Veiculo> listar() {
+
         try {
             EntityManager entityManager = Conexao.getEntityManager();
 
             TypedQuery<B_Veiculo> query = entityManager.createQuery("SELECT v FROM veiculos v", B_Veiculo.class);
-            
+
             List<B_Veiculo> listaVeiculos = query.getResultList();
-                   
+
             entityManager.close();
-            
+
             return listaVeiculos;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    
-    public B_Veiculo pesquizarPorID(Integer id){
-    
-    EntityManager entityManager = Conexao.getEntityManager();
-    
-    B_Veiculo manPesq = entityManager.find(B_Veiculo.class, id);
-    
-    entityManager.close();
-    
-    return manPesq;
+
+    public B_Veiculo pesquizarPorID(Integer id) {
+
+        EntityManager entityManager = Conexao.getEntityManager();
+
+        B_Veiculo manPesq = entityManager.find(B_Veiculo.class, id);
+
+        entityManager.close();
+
+        return manPesq;
     }
 }
--- src/java/Controller/MB_Veiculo.java
+++ src/java/Controller/MB_Veiculo.java
@@ -15,50 +15,64 @@ import javax.faces.bean.SessionScoped;
  *
  * @author jean.leal
  */
-@ManagedBean (name = "mbVeic")
+@ManagedBean(name = "mbVeic")
 @SessionScoped
 public class MB_Veiculo {
 
     private B_Veiculo veic;
     private List<B_Veiculo> veics;
     private boolean edit = false;
-    
+
     public MB_Veiculo() {
         veic = new B_Veiculo();
         this.veics = new ArrayList<B_Veiculo>();
         this.listarVeiculo();
     }
-    
+
     public void cadastraEeditaVeiculo() {
         if (edit) {
             int index = this.veics.lastIndexOf(this.veic);
             this.veics.set(index, veic);
-            //this.limpaCampos();
+            this.listarVeiculo();
             this.edit = false;
         } else {
             B_Veiculo v = new B_Veiculo(this.veic.getId(), 1, 1, 1, this.veic.getAnoFabricacao(), this.veic.getAnoModelo(), this.veic.getKm(), this.veic.getCor(), this.veic.getPlaca(), 1);
             BD_Veiculo ve = new BD_Veiculo();
             ve.salvar(v);
             this.listarVeiculo();
-            //this.limpaCampos();
             this.edit = false;
         }
     }
-    
-    public void excluiVeiculo(B_Veiculo v){
-        this.veic = v;
-        int index = this.veics.lastIndexOf(v);
-        this.veics.remove(index);
+
+    public void excluiVeiculo(B_Veiculo v) {
+        BD_Veiculo bdv = new BD_Veiculo();
+        bdv.excluir(v);
+        this.listarVeiculo();
     }
-    
-    public void editaVeiculo(B_Veiculo v){
+
+    public void editaVeiculo(B_Veiculo v) {
         this.veic = v;
         edit = true;
     }
-    
-    public void listarVeiculo(){
+
+    public void listarVeiculo() {
+        this.limpar();
         BD_Veiculo bdVeic = new BD_Veiculo();
         this.veics = bdVeic.listar();
+    }
+
+    public void limpar() {
+        this.veics.clear();
+        this.veic.setAnoFabricacao(null);
+        this.veic.setAnoModelo(null);
+        this.veic.setCor(null);
+        this.veic.setId(null);
+        this.veic.setIdMarca(null);
+        this.veic.setIdModelo(null);
+        this.veic.setIdTipo(null);
+        this.veic.setIdUsuario(null);
+        this.veic.setKm(null);
+        this.veic.setPlaca(null);
     }
 
     public B_Veiculo getVeic() {
